2ab0855fd2ecab9a01f7c738d9f3bbbefdab24d6
Juan C Nuno <juancnuno@gmail.com>
2024-09-20 12:41:05 -0700

    Add YearsWorthOfExpenses

---
+++ src/main/java/com/juancnuno/yearsworthofexpenses/YearsWorthOfExpenses.java
@@ -0,0 +1,11 @@
+package com.juancnuno.yearsworthofexpenses;
+
+final class YearsWorthOfExpenses {
+
+    private YearsWorthOfExpenses() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
