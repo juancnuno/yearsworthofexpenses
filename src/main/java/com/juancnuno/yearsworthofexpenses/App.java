53dfd0d1034c82ae562e5c04f9ce74caf89a754d
Juan C Nuno <juancnuno@gmail.com>
2024-09-20 12:15:39 -0700

    Commit yearsworthofexpenses

---
+++ src/main/java/com/juancnuno/yearsworthofexpenses/App.java
@@ -0,0 +1,10 @@
+package com.juancnuno.yearsworthofexpenses;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
